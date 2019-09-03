7d738ae1937308cead42b010aabb6e7c2994a6b9
Lucy <lucyneatdesign@yahoo.ca>
2019-09-03 00:48:10 +0000

    Fixed bugs #3

---
+++ public/build/precache-manifest.6fa5cd01b78b1a30df7bf8e875bf99f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80269fa090803fa032b1a06bd647f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85bde94ce5df2c989cf0",
+    "url": "/static/css/main.c7e76c7c.chunk.css"
+  },
+  {
+    "revision": "fb1820eb66c4f7461373",
+    "url": "/static/js/2.c8f4cf8e.chunk.js"
+  },
+  {
+    "revision": "85bde94ce5df2c989cf0",
+    "url": "/static/js/main.bf1920be.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "6e1767eb3d3e338ec34943ece03ebd26",
+    "url": "/static/media/dicaprio.6e1767eb.png"
+  },
+  {
+    "revision": "01de3b81fe45f91b9d5a008f5feb9d9b",
+    "url": "/static/media/logo_03.01de3b81.png"
+  }
+]);
